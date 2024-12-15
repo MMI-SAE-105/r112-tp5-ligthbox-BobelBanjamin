b232cc2793c4e810bec2d640c96f87399824cbe7
BobelBanjamin <banjamin.bobel@edu.univ-fcomte.fr>
2024-12-15 22:44:33 +0300

    code java fait avec copilot

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
 // *** LIGHTBOX ***
+
+const lb = document.querySelector("#lightbox");
+
+lb.addEventListener("click", function() {
+    lb.close();
+});
+
+const images = document.querySelectorAll("[data-full-img]");
+
+images.forEach(image => {
+    image.addEventListener("click", function() {
+        const fullImg = image.dataset.fullImg;
+        const imgElement = lb.querySelector("img");
+        imgElement.src = fullImg;
+        lb.showModal();
+    });
+});
